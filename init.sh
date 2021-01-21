8f6abe71f08948026c261c45cbfbf9d40c347942
Nedoluzhko Denis <denisnedoluzhko1902@gmail.com>
2021-01-21 21:31:25 +0000

    typos fixed

--- init.sh
+++ init.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-NGINX_CONF_LINK_DEFAULT=/etc/nginx/sies-enabled/default
+NGINX_CONF_LINK_DEFAULT=/etc/nginx/sites-enabled/default
 
 NGINX_CONF_LINK_NEW=/etc/nginx/sites-enabled/nginx.conf
 NGINX_CONF_FILE_NEW=/home/box/web/etc/nginx.conf
 
 if test -L $NGINX_CONF_LINK_DEFAULT; then
 	sudo unlink $NGINX_CONF_LINK_DEFAULT
-if
+fi
 
 sudo ln -s $NGINX_CONF_FILE_NEW $NGINX_CONF_LINK_NEW
 sudo /etc/init.d/nginx restart
