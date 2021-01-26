7859c7930e115fa8a07c623aa6b5f2a75ecdfcdf
Nedoluzhko Denis <denisnedoluzhko1902@gmail.com>
2021-01-26 18:46:58 +0300

    encoding for bytes func added and typo fixed

--- etc/hello.py
+++ etc/hello.py
@@ -1,11 +1,11 @@
-def wsgi_appication(environ, start_response):
+def wsgi_application(environ, start_response):
     status = "200 OK"
 
     headers = [
         ("Content-Type", "text/plain")
     ]
 
-    body = (bytes(variable + "\n")
+    body = (bytes(variable + "\n", "utf-8")
             for variable in environ["QUERY_STRING"].split('&'))
 
     start_response(status, headers)
