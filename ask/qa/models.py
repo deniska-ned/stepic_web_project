92ed271c0e1d1887e6eea72216f29ff507a35084
Nedoluzhko Denis <denisnedoluzhko1902@gmail.com>
2021-06-29 12:50:09 +0300

    the task is completed

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+    def popular(self):
+        return self.ordered_by('-rating')
+
+
+class Question(models.Model):
+    title = models.CharFiled(max_length=255)
+    text = models.TextFiled()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    likes = models.ManyToManyField(User, null=True, on_delete=models.SET_NULL)
+
+    objects = QuestionManager()
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(Question, null=False,
+            on_delete=models.SET_CASCADE)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
--- init.sh
+++ init.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-NGINX_CONF_LINK_DEFAULT=/etc/nginx/sites-enabled/default
 
-NGINX_CONF_LINK_NEW=/etc/nginx/sites-enabled/nginx.conf
-NGINX_CONF_FILE_NEW=/home/box/web/etc/nginx.conf
+# Mysql configuration
+
+sudo /etc/init.d/mysql start
+
+mysql -u root -e "CREATE DATABASE stepic_web_db;"
+mysql -u root -e "CREATE USER me IDENTIFIED BY 'password';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON * . * TO 'me'@'localhost';"
+
 
 # WSGI configuration
 
@@ -11,10 +16,18 @@ mkdir -p log
 gunicorn -b 0.0.0.0:8080 hello:wsgi_application >> log/gunicorn_hello.log 2>&1 &
 
 cd ask
+./manage.py makemigrations
+./manage.py migrate
 gunicorn -b 0.0.0.0:8000 ask.wsgi >> ../log/gunicorn_stackoverflow.log 2>&1 &
 cd ..
 
+
 # Nginx configuration
+
+NGINX_CONF_LINK_DEFAULT=/etc/nginx/sites-enabled/default
+
+NGINX_CONF_LINK_NEW=/etc/nginx/sites-enabled/nginx.conf
+NGINX_CONF_FILE_NEW=/home/box/web/etc/nginx.conf
 
 if test -L $NGINX_CONF_LINK_DEFAULT; then
 	sudo unlink $NGINX_CONF_LINK_DEFAULT
